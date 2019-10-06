4b514782af8002ba7a599141cd98e3baf55fc043
Vinh <vinhdlv@imt-soft.com>
2019-10-06 13:22:11 +0700

    product page

--- CuaHangTinHoc_Client/src/modules/product-detail-page/ProductDetailPage.vue
+++ CuaHangTinHoc_Client/src/modules/product-detail-page/ProductDetailPage.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-    asdasdasdasd
-  </div>  
+    <div class="row">
+      <div class="col-md-5" style="background-color:yellowgreen">
+        <div class="row">
+          <div class="col-md-2" style="background-color:red">list IMAGE</div>
+          <div class="col-md-10" style="background-color:blue">Big IMAGE</div>
+        </div>
+      </div>
+      <div class="col-md-7" style="background-color:bluesky">
+        <div class="row" style="background-color:yellow">
+          <div class="col">Title</div>
+        </div>
+        <div class="row">
+          <div class="col" style="background-color:silver">Price</div>
+        </div>
+        <div class="row" style="background-color:white">
+          <div class="col">Detail</div>
+        </div>
+        <div class="row">
+          <div class="col">Control</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-  
-}
+export default {};
 </script>
 <style scoped>
-
 </style>
