abd205d67ad016253ffe815988ac097ad888b8cf
Vinh <vinhdlv@imt-soft.com>
2019-11-23 00:49:11 +0700

    no message

--- cuahangtinhoc-Server-CNPMM/router.js
+++ cuahangtinhoc-Server-CNPMM/router.js
@@ -5,14 +5,11 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 let middleware = require('./middleware');
 let HomePageController = require('./controler/HomePageController');
 let StorePageController = require('./controler/StorePageController');
-<<<<<<< HEAD
 let ProductDetailController = require('./controler/ProductDetailController');
 
 router.get('/productType', HomePageController.productTypeList)
-=======
 let MyPageController = require('./controler/MyPageController');
 
->>>>>>> 7bfac1a22b6957f9d8813e4a7cd0569aef1848bf
 // product
 // product detail page
 router.get('/productDetails', ProductDetailController.productDetail)
@@ -36,7 +33,6 @@ router.get('/storeProduct',middleware.requireLogin, StorePageController.productL
 // store type product
 router.get('/storeProductType',middleware.requireLogin, StorePageController.productTypeList)
 
-<<<<<<< HEAD
 
 
 
@@ -61,10 +57,8 @@ router.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
 
 router.get('/auth/facebook/callback',
   passport.authenticate('facebook', { successRedirect : 'http://localhost:8081/mypage', failureRedirect: 'http://localhost:8081' }));
-=======
 // mypage
 router.get('/customerInfo', MyPageController.getCustomerInfo)
 router.get('/listOrders', MyPageController.getListOders)
 router.get('/orderDetail', MyPageController.detailOrder)
 router.get('/listBoughtProduct', MyPageController.getListBoughtProduct)
->>>>>>> 7bfac1a22b6957f9d8813e4a7cd0569aef1848bf
