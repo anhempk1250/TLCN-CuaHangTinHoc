f09c91513c854748325a213cb7dee00fe54e75e8
daolevanvinh <daolevanvinh@gmail.com>
2019-12-10 10:25:58 +0700

    no message

--- cuahangtinhoc-Server-CNPMM/controler/ProductDetailController.js
+++
@@ -1,8 +0,0 @@
-let Product = require('../model/Product');
-
-exports.productDetail = function(req, res) {
-    let id = req.query.id;
-    Product.findOne({id: id}).populate('images').exec(function(err,list){
-        res.json(list);
-    })
-}
--- cuahangtinhoc-Server-CNPMM/model/Image.js
+++
@@ -1,9 +0,0 @@
-let mongooes = require('mongoose')
-let schema = mongooes.Schema
-
-let image = new schema({
-  product_id: {type: schema.Types.ObjectId, ref: 'product'},
-  image_link : String,
-}, { collection: 'image' })
-
-module.exports = mongooes.model('image', image)
